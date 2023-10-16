1b9d87d0bbb85401fa25f2af3e2356ef9e035713
Tyuiu.KupriyanovEA@outlook.com <Tyuiu.KupriyanovEA@outlook.com>
2023-10-16 19:21:40 +0500

    Task0

---
+++ Tyuiu.KupriyanovEA.Sprint3.Task0.V7.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KupriyanovEA.Sprint3.Task0.V7.Lib
+{
+    public class DataService : ISprint3Task0V7
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+            int i = 0;
+            for ( i = startValue; i <= stopValue; i++)
+            {
+                sumSeries = sumSeries + ((Math.Pow(value, i) + 1) * Math.Cos(i));
+            }
+            double res = Math.Round(sumSeries, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KupriyanovEA.Sprint3.Task0.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KupriyanovEA.Sprint3.Task0.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KupriyanovEA.Sprint3.Task0.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99f570ec-93a1-41ec-a9d9-c3915b8ff500")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
